3cedd06c4176bb08c75ce742b557225d590f07f8
Olga Kunyavskaya <olga.kunyavskaya@aurora.dev>
2023-03-17 04:32:40 +0200

    `TransferMessage`: add an optional `aurora_sender` field (#10)

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use hex::FromHex;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::{
     json_types::U128, log, require, serde::Deserialize, serde::Serialize, serde_json, AccountId,
@@ -8,7 +9,31 @@ pub const STANDARD: &str = "nep297";
 pub const VERSION: &str = "1.0.0";
 pub const EVENT_JSON_STR: &str = "EVENT_JSON:";
 
-pub type EthAddress = [u8; 20];
+#[derive(BorshDeserialize, BorshSerialize, Debug, Clone, Copy, PartialEq)]
+pub struct EthAddress(pub [u8; 20]);
+
+impl<'de> Deserialize<'de> for EthAddress {
+    fn deserialize<D>(deserializer: D) -> Result<Self, <D as serde::Deserializer<'de>>::Error>
+        where
+            D: serde::Deserializer<'de>,
+    {
+        let mut s = <String as Deserialize>::deserialize(deserializer)?;
+        if s.starts_with("0x") {
+            s = s[2..].to_string();
+        }
+        let result = Vec::from_hex(&s).map_err(|err| serde::de::Error::custom(err.to_string()))?;
+        Ok(EthAddress(result.try_into().unwrap()))
+    }
+}
+
+impl Serialize for EthAddress {
+    fn serialize<S>(&self, serializer: S) -> Result<<S as serde::Serializer>::Ok, <S as serde::Serializer>::Error>
+        where
+            S: serde::Serializer,
+    {
+        serializer.serialize_str(&hex::encode(self.0))
+    }
+}
 
 #[derive(
     Default, BorshDeserialize, BorshSerialize, Debug, Clone, Serialize, Deserialize, PartialEq,
@@ -26,7 +51,6 @@ pub struct Proof {
 #[serde(crate = "near_sdk::serde")]
 pub struct TransferDataEthereum {
     pub token_near: AccountId,
-    #[serde(with = "hex::serde")]
     pub token_eth: EthAddress,
     pub amount: U128,
 }
@@ -44,9 +68,9 @@ pub struct TransferMessage {
     pub valid_till: u64,
     pub transfer: TransferDataEthereum,
     pub fee: TransferDataNear,
-    #[serde(with = "hex::serde")]
     pub recipient: EthAddress,
     pub valid_till_block_height: Option<u64>,
+    pub aurora_sender: Option<EthAddress>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
@@ -88,7 +112,7 @@ pub fn get_eth_address(address: String) -> EthAddress {
     let data = hex::decode(address)
         .unwrap_or_else(|_| near_sdk::env::panic_str("address should be a valid hex string."));
     require!(data.len() == 20, "address should be 20 bytes long");
-    data.try_into().unwrap()
+    EthAddress(data.try_into().unwrap())
 }
 
 pub fn remove_prefix(event_str: &str) -> std::option::Option<serde_json::Value> {
@@ -168,12 +192,13 @@ mod tests {
                     amount: U128(amount),
                 },
                 recipient: get_eth_address(),
+                aurora_sender: Some(EthAddress(<[u8;20]>::default())),
             },
         }
         .emit();
 
         let log_data_str = &test_utils::get_logs()[0];
-        let expected_result_str = r#"EVENT_JSON:{"standard":"nep297","version":"1.0.0","event":"fast_bridge_init_transfer_event","data":{"nonce":"238","sender_id":"sender.near","transfer_message":{"valid_till":0,"valid_till_block_height":0,"transfer":{"token_near":"token.near","token_eth": "71c7656ec7ab88b098defb751b7401b5f6d8976f","amount":"100"},"fee":{"token":"token.near","amount":"100"},"recipient": "71c7656ec7ab88b098defb751b7401b5f6d8976f"}}}"#;
+        let expected_result_str = r#"EVENT_JSON:{"standard":"nep297","version":"1.0.0","event":"fast_bridge_init_transfer_event","data":{"nonce":"238","sender_id":"sender.near","transfer_message":{"aurora_sender":"0000000000000000000000000000000000000000","valid_till":0,"valid_till_block_height":0,"transfer":{"token_near":"token.near","token_eth": "71c7656ec7ab88b098defb751b7401b5f6d8976f","amount":"100"},"fee":{"token":"token.near","amount":"100"},"recipient": "71c7656ec7ab88b098defb751b7401b5f6d8976f"}}}"#;
 
         let json1 = remove_prefix(log_data_str).unwrap();
         let json2 = remove_prefix(expected_result_str).unwrap();
@@ -204,12 +229,13 @@ mod tests {
                     amount: U128(amount),
                 },
                 recipient: get_eth_address(),
+                aurora_sender: Some(EthAddress(<[u8;20]>::default())),
             },
         }
         .emit();
 
         let log_data_str = &test_utils::get_logs()[0];
-        let expected_result_str = r#"EVENT_JSON:{"standard":"nep297","version":"1.0.0","event":"fast_bridge_unlock_event","data":{"nonce":"238","recipient_id":"recipient.near","transfer_message":{"valid_till":0,"valid_till_block_height":0,"transfer":{"token_near":"token.near","token_eth": "71c7656ec7ab88b098defb751b7401b5f6d8976f","amount":"100"},"fee":{"token":"token.near","amount":"100"},"recipient": "71c7656ec7ab88b098defb751b7401b5f6d8976f"}}}"#;
+        let expected_result_str = r#"EVENT_JSON:{"standard":"nep297","version":"1.0.0","event":"fast_bridge_unlock_event","data":{"nonce":"238","recipient_id":"recipient.near","transfer_message":{"aurora_sender":"0000000000000000000000000000000000000000","valid_till":0,"valid_till_block_height":0,"transfer":{"token_near":"token.near","token_eth": "71c7656ec7ab88b098defb751b7401b5f6d8976f","amount":"100"},"fee":{"token":"token.near","amount":"100"},"recipient": "71c7656ec7ab88b098defb751b7401b5f6d8976f"}}}"#;
 
         let json1 = remove_prefix(log_data_str).unwrap();
         let json2 = remove_prefix(expected_result_str).unwrap();
